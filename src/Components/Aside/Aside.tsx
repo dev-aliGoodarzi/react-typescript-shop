160b4e859474cd466e43b52d725af594df162e95
M-r-hydra <me74242@gmail.com>
2022-08-22 15:12:46 +0430

    starting building ASIDE

--- src/Components/Aside/Aside.tsx
+++ src/Components/Aside/Aside.tsx
@@ -6,7 +6,9 @@ import React from "react";
 import styles from "./Aside.module.css";
 // CSS
 const aside = () => {
-  return <div className={styles.asideContainer}></div>;
+  return <div className={styles.asideContainer}>
+    
+  </div>;
 };
 
 export default aside;
